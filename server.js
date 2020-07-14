0f40015f3bd885d1e831b0d864abe565163e9435
Lucas <lemmg@cin.ufpe.br>
2020-07-14 12:20:41 -0300

    Teste tela confirma03

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ server.post("/", function(req, res){
 
 //============ Rota do Culto da Segunda ================
 
-server.post("/CultoSegunda", "/telaConfirma", function(req, res){
+server.post("/CultoSegunda", function(req, res){
 	//pegar dados do formulário
 	const nome = req.body.nome
 	const cpfSegunda = req.body.cpfSegunda
