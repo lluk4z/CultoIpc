e851048772f70b96808272c7668c647c6ea03a15
Lucas <lemmg@cin.ufpe.br>
2020-07-28 16:37:57 -0300

    Validar telefone plano b CORRECAO

--- server.js
+++ server.js
@@ -66,20 +66,6 @@ Soma = 0;
 
 // Função para validar o telefone
 
-function phoneValidate($phone)
-    {
-        $regex = '/^(?:(?:\+|00)?(55)\s?)?(?:\(?([1-9][0-9])\)?\s?)?(?:((?:9\d|[2-9])\d{3})\-?(\d{4}))$/';
-
-        if (preg_match($regex, $phone) == false) {
-
-            // O número não foi validado.
-            return false;
-        } else {
-
-            // Telefone válido.
-            return true;
-        }        
-    }
 
 //================================
 
